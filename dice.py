1c448c2fc4bcffffc8bd633e5de8f5398c08e2ef
lokilaufeson <prashanthkgp1@gmail.com>
2018-11-09 00:46:05 +0530

    new

--- dice.py
+++ dice.py
@@ -2,7 +2,8 @@ import random
 i=1
 def roll():
   return random.randint(1,6)
-while(i):
-  print(roll())
-  print("roll again?")
-  i=int(input())
+'''while(i):
+  print(roll())'''
+  
+print("roll again?")
+  '''i=int(input())''''
